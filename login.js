6655b518eff049d4d73ba8c798fd9bb7ec31a67a
sjsinaga94 {Satria Sinaga} <67063994+satria94@users.noreply.github.com>
2020-06-29 19:46:38 +0700

    Create login.js

---
+++ login.js
@@ -0,0 +1,31 @@
+function onSignIn(googleUser) {
+	var profile = googleUser.getBasicProfile();
+	console.log('Fullname: ' + profile.getName());
+	console.log("Email:" + profile.getEmail());
+
+	var x = document.getElementsByClassName("logged");
+	var i;
+	for (i =0; i <= x.length; i++) {
+		x[0].classList.remove("logged");
+	}
+}
+
+function onSignOut() {
+	var auth2 = gapi.auth2.getAuthInstance();
+	auth2.signout().then(function () {
+		console.log('User signed out');
+		hideDashboard();
+	} );
+}
+
+function hideDashboard(){
+var x= document.getElementsByClassName("onlylogged");
+var i;
+for(i =0; i <= x.length; i++) {
+	x[i].classList.add("logged");
+	}
+}
+
+function gotoDashboard(){
+	window.location.replace("http://localhost:8080/feed.html")
+}
